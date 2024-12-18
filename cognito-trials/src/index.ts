277b92a252abe75d3d62be4f840f80b6b9e5af64
alexhddev <21276888+alexhddev@users.noreply.github.com>
2024-12-18 15:56:20 +0200

    stuff

--- cognito-trials/src/index.ts
+++ cognito-trials/src/index.ts
@@ -1,9 +1,17 @@
-import { getSession, login } from './login'
+import { getSession, login } from './login.ts'
 
-async function main(){
-    const result = await login('test', 'test')
+async function main() {
+    const result = await login('barosanu', 'Isdufhie354$')
+    console.log('result: ')
+    console.log(JSON.stringify(result, null, 2))
+
     const session = await getSession();
-    const a = 5;
+    console.log('session: ')
+    console.log(JSON.stringify(session, null, 2))
+
+    console.log('How a token looks: ')
+    const idToken = session.tokens?.accessToken.toString();
+    console.log(idToken)
 }
 
 main()
