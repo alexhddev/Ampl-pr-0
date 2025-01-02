cb3eb2cceaf599c867f0c2ad33fcfd01890339e8
alexhddev <21276888+alexhddev@users.noreply.github.com>
2025-01-02 10:57:03 +0200

    stuff

--- cognito-trials2/src/test.ts
+++ cognito-trials2/src/test.ts
@@ -1,4 +1,4 @@
-import { getSession, login, generateTemporaryCredentials, generateTemporaryGuestCredentials } from './Auth.ts'
+import { getSession, login } from './Auth.ts'
 import config from '../config.json'
 
 async function main() {
@@ -16,15 +16,15 @@ async function main() {
     const idToken = session.tokens?.idToken?.toString();
     console.log(idToken)
 
-    if (idToken) {
-        console.log('Getting temporary credentials for authenticated user: ')
-        const authCredentials = await generateTemporaryCredentials(idToken)
-        console.log(authCredentials)
-    }
+    // if (idToken) {
+    //     console.log('Getting temporary credentials for authenticated user: ')
+    //     const authCredentials = await generateTemporaryCredentials(idToken)
+    //     console.log(authCredentials)
+    // }
 
-    console.log('Getting temporary credentials for guest user: ')
-    const authCredentials = await generateTemporaryGuestCredentials()
-    console.log(authCredentials)
+    // console.log('Getting temporary credentials for guest user: ')
+    // const authCredentials = await generateTemporaryGuestCredentials()
+    // console.log(authCredentials)
 }
 
 export async function test(element: HTMLButtonElement) {
