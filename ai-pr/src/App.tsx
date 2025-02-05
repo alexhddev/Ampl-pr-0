323304b6a260c7b1b189b4a2a812ada4e6d836f1
alexhddev <21276888+alexhddev@users.noreply.github.com>
2025-02-05 20:36:32 +0200

    stuff

--- ai-pr/src/App.tsx
+++ ai-pr/src/App.tsx
@@ -1,33 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+// import './App.css'
+import { Authenticator } from '@aws-amplify/ui-react'
+import { AIConversation, createAIHooks } from '@aws-amplify/ui-react-ai'
+import { generateClient } from "aws-amplify/api";
+import { Schema } from "../amplify/data/resource";
+import '@aws-amplify/ui-react/styles.css';
+
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  const client = generateClient<Schema>({ authMode: "userPool" }); //{ authMode: "userPool" }
+  const { useAIConversation, useAIGeneration } = createAIHooks(client);
+
+  const conversation = useAIConversation('chat')
+  const messages = conversation[0].data.messages;
+  const isLoading = conversation[0].isLoading;
+  const sendMessage = conversation[1];
+
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Authenticator>
+        <AIConversation
+          messages={messages}
+       //   isLoading={isLoading}
+          handleSendMessage={sendMessage}
+
+        ></AIConversation>
+      </Authenticator>
     </>
   )
 }
