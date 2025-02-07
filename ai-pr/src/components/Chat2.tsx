aa1f8da27de8f5e5a902f1c0d371bf71ea121029
alexhddev <21276888+alexhddev@users.noreply.github.com>
2025-02-07 17:14:06 +0200

    add weather card

--- ai-pr/src/components/Chat2.tsx
+++ ai-pr/src/components/Chat2.tsx
@@ -18,9 +18,46 @@ function Chat() {
         <AIConversation
             messages={messages}
             handleSendMessage={sendMessage}
+            responseComponents={{
+                WeatherCard: {
+                    description: 'Used to display the weather of a given city to the user',
+                    // any React component can be used
+                    component: (props) => {
+                        return <Card
+                            {...props}
+                             />;
+                    },
+                    props: {
+                        city: {
+                            type: 'string',
+                            required: true,
+                        },
+                        temperature: {
+                            type: 'string'
+                        },
+                        unit: {
+                            type: 'string'
+                        }
+                    },
+                },
+            }}
         />
 
     </main>
+}
+
+function Card(props: {
+    city: string,
+    temperature: string,
+    unit: string
+}) {
+
+    console.log('Card props: ')
+    console.log(props)
+    return <div style={{ border: '1px solid black' }}>
+        <h1>{props.city}</h1>
+        <h2>{props.temperature} {props.unit}</h2>
+    </div>
 }
 
 export default Chat
