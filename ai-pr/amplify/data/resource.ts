dc429a2256b6f16629a06fdcbc81bf39cff5b888
alexhddev <21276888+alexhddev@users.noreply.github.com>
2025-02-07 13:12:43 +0200

    function calling

--- ai-pr/amplify/data/resource.ts
+++ ai-pr/amplify/data/resource.ts
@@ -1,15 +1,29 @@
 import { type ClientSchema, a, defineData } from '@aws-amplify/backend';
+import { getWeather } from '../functions/weather/resource';
 
 const schema = a.schema({
+  getWeather: a.query()
+    .arguments({city: a.string().required()})
+    .returns(a.customType({
+      value: a.float().required(),
+      unit: a.string().required(),
+    }))
+    .handler(a.handler.function(getWeather))
+    .authorization((allow) => allow.authenticated()),
   chat: a.conversation({
-    aiModel: a.ai.model('Claude 3.5 Sonnet'),
-    systemPrompt: 'You are helpful assistant'
+    aiModel: a.ai.model('Claude 3 Haiku'),
+    systemPrompt: 'You are helpful assistant',
+    tools: [{
+      name: 'getWeatherTool',
+      description: 'Provide the current weather for the city',
+      query: a.ref('getWeather')
+    }]
   }).authorization(
     (allow) => allow.owner()
   ),
     
   generateRecipe: a.generation({
-    aiModel: a.ai.model('Claude 3.5 Sonnet'),
+    aiModel: a.ai.model('Claude 3 Haiku'),
     systemPrompt: 'You are a helpful assistant that generates recipes.',
   })
   .arguments({
